df9fbf0b413fc31324545fa8fc0e4a011262dbfb
Sebastian Wilzbach <seb@wilzba.ch>
2017-12-04 21:53:37 +0100

    Improve watcher test

--- test/watch.sh
+++ test/watch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-TEST_DIR=$(dirname $0)
-echo 'enum count = 0;' > "$TEST_DIR"/watch/source/counter.d
+TEST_DIR="$(dirname "$0")"
+COUNTER="$TEST_DIR"/watch/source/counter.d
+echo 'enum count = 0;' > "$COUNTER"
 # need force here, because the modtime change is too small
-$DUB watch --root="$TEST_DIR"/watch --compiler=$COMPILER --force
+$DUB watch --root="$TEST_DIR/watch" --compiler="$COMPILER" --force
+if [ "$(cat "$COUNTER")" != "enum count = 3;" ] ; then
+    echo "Invalid counter state."
+    echo "Counter is: $(cat "$COUNTER")"
+    exit 1
+fi
