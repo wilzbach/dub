6c17ca913ccd7de26d5525822766a9299427116c
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-10 17:15:14 +0100

    Experiment with GH deploys

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -v -e -o pipefail
+
+DFLAGS="-release" DMD="ldmd" ./build.sh
+VERSION=$(git describe --abbrev=0 --tags)
+
+unameOut="$(uname -s)"
+case "$unameOut" in
+    Linux*) OS=linux; ;;
+    OSX*) OS=osx; ;;
+    *) echo "Unknown OS: $unameOut"; exit 1
+esac
+
+archiveName="dub-$VERSION-$OS-x86_64.tar.gz"
+
+echo "Building $archiveName"
+tar cvfz "bin/$archiveName" -C bin dub
+
+# Set latest version, s.t. GH_PAGES get updated
+mkdir -p docs
+echo $VERSION docs/LATEST
