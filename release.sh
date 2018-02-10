5c683102979f4972bb0ce8a6589a291158d52922
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-10 17:15:14 +0100

    Add automatic GH deployment for Linux 32/64 and OSX 64

---
+++ release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -v -e -o pipefail
+
+VERSION=$(git describe --abbrev=0 --tags)
+ARCH="${ARCH:-64}"
+
+unameOut="$(uname -s)"
+case "$unameOut" in
+    Linux*) OS=linux; ;;
+    Darwin*) OS=osx; ;;
+    *) echo "Unknown OS: $unameOut"; exit 1
+esac
+
+case "$ARCH" in
+    64) ARCH_SUFFIX="x86_64";;
+    32) ARCH_SUFFIX="x86";;
+    *) echo "Unknown ARCH: $ARCH"; exit 1
+esac
+
+archiveName="dub-$VERSION-$OS-$ARCH.tar.gz"
+
+echo "Building $archiveName"
+DFLAGS="-release -m$ARCH" DMD="$(command -v $DMD)" ./build.sh
+tar cvfz "bin/$archiveName" -C bin dub
+
+#if [ "$OS" == "linux" ] ; then
+    #archiveName="dub-$VERSION-$OS-x86.tar.gz"
+    #echo "Building $archiveName"
+    #DFLAGS="-release -m32" DMD="$(command -v ldmd2)" ./build.sh
+    #tar cvfz "bin/$archiveName" -C bin dub
+#fi
+
+# Set latest version, s.t. GH_PAGES get updated
+#mkdir -p docs
+#echo $VERSION > docs/LATEST
